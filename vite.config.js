3278183bdffec3106f7f8f34c9691cb279ea86a4
ActionHudson <Marc.h.w.hudson@gmail.com>
2025-09-14 21:21:25 +0100

    fix routes and vite.config for local dev and github pages

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,29 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-    plugins: [react()],
-    base: "/ReactApp/",
-    server: {
-        port: 3000
-    },
-    test: {
-        globals: true,
-        environment: "jsdom",
-        setupFiles: "./setupTests.js",
-        transformMode: {
-            web: [/.[jt]sx?/]
+/** @param {{ mode: string }} env */
+export default defineConfig(env => {
+    const { mode } = env;
+
+    return {
+        plugins: [react()],
+        base: mode === "production" ? "/ReactApp/" : "/",
+        server: {
+            port: 3000
         },
-        include: ["src/**/*.{test.js,test.jsx,test.ts,test.tsx}"],
-        coverage: {
-            provider: "v8",
-            reporter: [ "text", "json", "html", "lcov" ],
-            reportsDirectory: "./coverage",
-            include: ["src/**/*.{js,jsx,ts,tsx}"],
-            exclude: [ "src/**/*.test.{js,jsx,ts,tsx}", "**/*.stories.*", "**/setupTests.{js,ts}" ]
+        test: {
+            globals: true,
+            environment: "jsdom",
+            setupFiles: "./setupTests.js",
+            transformMode: { web: [/.[jt]sx?/] },
+            include: ["src/**/*.{test.js,test.jsx,test.ts,test.tsx}"],
+            coverage: {
+                provider: "v8",
+                reporter: [ "text", "json", "html", "lcov" ],
+                reportsDirectory: "./coverage",
+                include: ["src/**/*.{js,jsx,ts,tsx}"],
+                exclude: [ "src/**/*.test.{js,jsx,ts,tsx}", "**/*.stories.*", "**/setupTests.{js,ts}" ]
+            }
         }
-    }
+    };
 });
