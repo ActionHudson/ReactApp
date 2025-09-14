f9608c8107119ba6b8038ed4d476cedcd4fc426b
ActionHudson <Marc.h.w.hudson@gmail.com>
2025-09-14 21:08:53 +0100

    fix vite config install gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,26 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [react()],
-    baseL "/ReactApp/"
+    base: "/ReactApp/",
     server: {
         port: 3000
     },
     test: {
         globals: true,
-        environment: 'jsdom',
-        setupFiles: './setupTests.js',
+        environment: "jsdom",
+        setupFiles: "./setupTests.js",
         transformMode: {
             web: [/.[jt]sx?/]
         },
-        include: ['src/**/*.{test.js,test.jsx,test.ts,test.tsx}'],
+        include: ["src/**/*.{test.js,test.jsx,test.ts,test.tsx}"],
         coverage: {
-            provider: 'v8',
-            reporter: [ 'text', 'json', 'html', 'lcov' ],
-            reportsDirectory: './coverage',
-            include: ['src/**/*.{js,jsx,ts,tsx}'],
-            exclude: [
-                'src/**/*.test.{js,jsx,ts,tsx}',
-                '**/*.stories.*',
-                '**/setupTests.{js,ts}'
-            ]
-
+            provider: "v8",
+            reporter: [ "text", "json", "html", "lcov" ],
+            reportsDirectory: "./coverage",
+            include: ["src/**/*.{js,jsx,ts,tsx}"],
+            exclude: [ "src/**/*.test.{js,jsx,ts,tsx}", "**/*.stories.*", "**/setupTests.{js,ts}" ]
         }
     }
 });
