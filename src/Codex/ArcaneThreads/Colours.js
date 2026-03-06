fba6d98c04f3e9de9b28f962cfccff086fc1beac
ActionHudson <Marc.h.w.hudson@gmail.com>
2026-03-06 21:36:01 +0000

    remove spaces - test to see if commit and push works

--- src/Codex/ArcaneThreads/Colours.js
+++ src/Codex/ArcaneThreads/Colours.js
@@ -2,18 +2,14 @@ export const Colours = {
     primary: "#0F1A20",
     muted: "#364148",
     disabled: "#868e96",
-
     secondary: "#FFFFFF",
-
     background: "#E4E4E4",
     border: "#C4C4C4",
-
     accent: {
         primary: "#FF6B9D",
         primaryHover: "#D7467B",
         secondary: "#F18F01"
     },
-
     status: {
         success: "#27B635",
         info: "#00A1FF",
